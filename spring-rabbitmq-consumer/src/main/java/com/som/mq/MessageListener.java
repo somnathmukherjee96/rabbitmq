e6991db54ef7ff13f42c8cadafdcf0c092158673
Somnath Mukherjee <somnathmukherjee@Somnaths-MBP.bbrouter>
2024-05-21 20:35:49 +0530

    rabbitmq demo

---
+++ spring-rabbitmq-consumer/src/main/java/com/som/mq/MessageListener.java
@@ -0,0 +1,12 @@
+package com.som.mq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void listener(CustomMessage customMessage){
+        System.out.println(customMessage);
+    }
+}
